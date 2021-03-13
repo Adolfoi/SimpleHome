5d8949450b24f8d3be1587eb90e63b3198855764
Mitsuhiro Hashimoto <adolfoi.ios.developer@gmail.com>
2021-03-14 08:22:20 +0900

    Add files via upload

--- Tweak.mm
+++ Tweak.mm
@@ -28,96 +28,22 @@
 @end
 
 
-
-/*
-%hook SpringBoard
-
--(void)applicationDidFinishLaunching:(id)application {
-
-    %orig;
-    UIWindow *foundWindow = nil;
-    NSArray *windows = [[UIApplication sharedApplication]windows];
-    for (UIWindow *window in windows) {
-        if (window.isKeyWindow) {
-            foundWindow = window;
-            break;
-        }
-    }
-    
-    UIViewController *view = foundWindow.rootViewController;
-
-        while (view.presentedViewController != nil && !view.presentedViewController.isBeingDismissed) {
-
-                view = view.presentedViewController;
-
-        }
-
-    UIAlertController *alertController =
-
-    [UIAlertController alertControllerWithTitle:@"ピカチュウ"
-
-                                        message:@"ゲットだぜ！！"
-
-                                 preferredStyle:UIAlertControllerStyleAlert];
-
-    [alertController addAction:[UIAlertAction actionWithTitle:@"はい"
-
-                                                        style:UIAlertActionStyleDefault
-
-                                                      handler:^(UIAlertAction *action) {
-
-        //ボタンが押された時の処理
-
-    }]];
-
-    [alertController addAction:[UIAlertAction actionWithTitle:@"いいえ"
-
-                                                        style:UIAlertActionStyleDefault
-
-                                                      handler:^(UIAlertAction *action) {
-
-        //ボタンが押された時の処理
-
-    }]];
-
-    [view presentViewController:alertController animated:YES completion:nil];
-    
-
-}
-
-%end
-*/
-
-
 %hook SBIconController
+/*
 // Today Viewを消すコード
 -(void)setTodayViewController:(SBHomeScreenTodayViewController *)arg1{
     %orig(nil);
 }
 
-
-
-
 // LibraryViewを消すコード
 -(void)setLibraryViewController:(SBHLibraryViewController *)arg1{
  %orig(nil);
 }
+*/
 
- 
-// TodayViewとLibraryView療法を消すコード
-/*
+// TodayViewとLibraryViewの両方を消すコード
 -(void)setHomeScreenOverlayController:(SBHomeScreenOverlayController *)arg1{
     %orig(nil);
 }
-*/
 
-
-/*
-%hook _UIStatusBarStringView
-- (void)setText:(NSString *)arg1 {
-    NSString *text = @"YOASOBI";
-    %orig(text);
-}
-
-*/
 %end
